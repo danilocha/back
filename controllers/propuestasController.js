f9d82fd81ed9c76e2ebc3ea16c750d88bf8436a1
danilocha <danilocha1@gmail.com>
2020-04-04 18:40:42 -0500

    cambiando propuestasController

--- controllers/propuestasController.js
+++ controllers/propuestasController.js
@@ -3,90 +3,89 @@ const puppeteer = require("puppeteer");
 
 // Muestra todos los productos
 exports.mostrarPropuestas = async (req, res, next) => {
-  // const id = req.params.id;
-  // // creo el navegador
-  // const browser = await puppeteer.launch();
-  // // abro la pagina que deseo analizar y capturar informacion
-  // const page = await browser.newPage();
-  // async function getPageData() {
-  //   await page.goto(
-  //     `https://www.grants.gov/custom/viewOppDetails.jsp?oppId=${id}`
-  //   );
-  //   await setTimeout(async () => {
-  //     const data = await page.evaluate(async () => {
-  //       //Analisis de la seccion General Information
-  //       const $detallesIzq = document.querySelectorAll(
-  //         "#synopsisDetailsGeneralInfoTableLeft > table > tbody >tr"
-  //       );
-  //       const $detallesDer = document.querySelectorAll(
-  //         "#synopsisDetailsGeneralInfoTableRight > table > tbody >tr"
-  //       );
-  //       const data = [];
-  //       // recorro los detalles de la izquiera
-  //       await $detallesIzq.forEach(($detalle) => {
-  //         data.push({
-  //           titulo: $detalle.querySelector(".label").textContent.trim(),
-  //           contenido: $detalle
-  //             .querySelector(".search-custom")
-  //             .textContent.trim(),
-  //         });
-  //       });
-  //       // recorro los detalles de la derecha
-  //       await $detallesDer.forEach(($detalle) => {
-  //         data.push({
-  //           titulo: $detalle.querySelector(".label").textContent.trim(),
-  //           contenido: $detalle
-  //             .querySelector(".search-custom")
-  //             .textContent.trim(),
-  //         });
-  //       });
+  const id = req.params.id;
+  // creo el navegador
+  const browser = await puppeteer.launch();
+  // abro la pagina que deseo analizar y capturar informacion
+  const page = await browser.newPage();
+  async function getPageData() {
+    await page.goto(
+      `https://www.grants.gov/custom/viewOppDetails.jsp?oppId=${id}`
+    );
+    await setTimeout(async () => {
+      const data = await page.evaluate(async () => {
+        //Analisis de la seccion General Information
+        const $detallesIzq = document.querySelectorAll(
+          "#synopsisDetailsGeneralInfoTableLeft > table > tbody >tr"
+        );
+        const $detallesDer = document.querySelectorAll(
+          "#synopsisDetailsGeneralInfoTableRight > table > tbody >tr"
+        );
+        const data = [];
+        // recorro los detalles de la izquiera
+        await $detallesIzq.forEach(($detalle) => {
+          data.push({
+            titulo: $detalle.querySelector(".label").textContent.trim(),
+            contenido: $detalle
+              .querySelector(".search-custom")
+              .textContent.trim(),
+          });
+        });
+        // recorro los detalles de la derecha
+        await $detallesDer.forEach(($detalle) => {
+          data.push({
+            titulo: $detalle.querySelector(".label").textContent.trim(),
+            contenido: $detalle
+              .querySelector(".search-custom")
+              .textContent.trim(),
+          });
+        });
 
-  //       // Analisis de la segunda seccion
+        // Analisis de la segunda seccion
 
-  //       const $detallesElegibility = document.querySelectorAll(
-  //         "#synopsisDetailsEligibilityTable > table > tbody >tr"
-  //       );
+        const $detallesElegibility = document.querySelectorAll(
+          "#synopsisDetailsEligibilityTable > table > tbody >tr"
+        );
 
-  //       const elegibility = [];
-  //       await $detallesElegibility.forEach(($detalle) => {
-  //         elegibility.push({
-  //           titulo: $detalle.querySelector(".label").textContent.trim(),
-  //           contenido: $detalle
-  //             .querySelector(".search-custom")
-  //             .textContent.trim(),
-  //         });
-  //       });
+        const elegibility = [];
+        await $detallesElegibility.forEach(($detalle) => {
+          elegibility.push({
+            titulo: $detalle.querySelector(".label").textContent.trim(),
+            contenido: $detalle
+              .querySelector(".search-custom")
+              .textContent.trim(),
+          });
+        });
 
-  //       // Analisis tercera seccion
-  //       const $detallesAdditionalInformation = document.querySelectorAll(
-  //         "#synopsisDetailsAdditionalInfoTable > table > tbody >tr"
-  //       );
+        // Analisis tercera seccion
+        const $detallesAdditionalInformation = document.querySelectorAll(
+          "#synopsisDetailsAdditionalInfoTable > table > tbody >tr"
+        );
 
-  //       const additionalInformation = [];
-  //       await $detallesAdditionalInformation.forEach(($detalle) => {
-  //         additionalInformation.push({
-  //           titulo: $detalle.querySelector(".label").textContent.trim(),
-  //           contenido: $detalle
-  //             .querySelector(".search-custom")
-  //             .textContent.trim(),
-  //         });
-  //       });
+        const additionalInformation = [];
+        await $detallesAdditionalInformation.forEach(($detalle) => {
+          additionalInformation.push({
+            titulo: $detalle.querySelector(".label").textContent.trim(),
+            contenido: $detalle
+              .querySelector(".search-custom")
+              .textContent.trim(),
+          });
+        });
 
-  //       return {
-  //         generalInfo: data,
-  //         elegibility,
-  //         additionalInformation,
-  //       };
-  //     });
-  //     res.json(data);
-  //   }, 2000);
-  res.json({ hola: "mundo" });
-  // }
+        return {
+          generalInfo: data,
+          elegibility,
+          additionalInformation,
+        };
+      });
+      res.json(data);
+    }, 2000);
+  }
 
-  // await getPageData();
-  // await setTimeout(() => {
-  //   browser.close();
-  // }, 30000);
+  await getPageData();
+  await setTimeout(() => {
+    browser.close();
+  }, 10000);
 };
 exports.idsPropuestas = async (req, res) => {
   const datos = [];
