91ee5bbacd8788873cc53b25e5aa45ef984e91a5
danilocha <danilocha1@gmail.com>
2020-04-05 10:37:19 -0500

    cambiando puertos y host

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const propuestasController = require("../controllers/propuestasController");
 
 module.exports = () => {
   // Mostrar todos los productos
+  router.get("/", (req, res) =>
+    res.json({
+      mensaje: "bienvenido",
+    })
+  );
   router.get("/propuestas/:id", propuestasController.mostrarPropuestas);
   router.get("/ids", propuestasController.idsPropuestas);
 
